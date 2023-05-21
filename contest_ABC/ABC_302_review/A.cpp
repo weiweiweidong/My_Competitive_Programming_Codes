50488e9b56bdc4298d40030703d2e6e4b16c4066
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2023-05-21 23:39:45 +0900

    ABC_302 problem A finished

---
+++ contest_ABC/ABC_302_review/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <math.h>
+#include <string.h>
+
+typedef long L;
+typedef long long LL;
+using namespace std;
+
+int main(){
+    LL a , b;
+    cin >> a >> b;
+    cout << (a+b-1)/b << endl;
+}
