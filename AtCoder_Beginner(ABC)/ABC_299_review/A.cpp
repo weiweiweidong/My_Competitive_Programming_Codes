9b49ebeb3e14bfb16a10910ec66c6af0097358ed
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2024-02-22 16:04:01 +0900

    ABC299， ABCE Finished

--- AtCoder_Beginner(ABC)/ABC_299_review/A.cpp
+++ AtCoder_Beginner(ABC)/ABC_299_review/A.cpp
@@ -17,6 +17,22 @@ typedef long long LL;
 using namespace std;
 
 int main() {
-    int n; cin >> n;
-    cout << n << endl;
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+
+    vector<int> t;
+    int x;
+    for (int i = 0; i < N; i++) {
+        if (S[i] == '|')
+            t.push_back(i);
+        if (S[i] == '*')
+            x = i;
+    }
+
+    if (x > t[0] && x < t[1])
+        cout << "in" << endl;
+    else
+        cout << "out" << endl;
 }
