055ed3b7df32c36a41fb29f35493365d9f245b04
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2024-03-16 20:33:23 +0900

    Template Update

--- 999_template/ABC_xxx/D.cpp
+++ 999_template/ABC_xxx/D.cpp
@@ -1,7 +1,15 @@
+// Problem:
+
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long LL;
 typedef pair<int, int> PII;
 
-int main() {}
+void solve() {}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    solve();
+    return 0;
+}
