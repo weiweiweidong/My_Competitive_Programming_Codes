a465db7322a67bc2dabe63786af9dc70c30f05fc
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2024-03-22 23:26:39 +0900

    Combination Problem

---
+++ 2_AcWing/1_Basic_Algorithms/CH4/7_Combination/1_ACW_885.cpp
@@ -0,0 +1,37 @@
+// Problem: https://www.acwing.com/problem/content/887/
+// 求组合数
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+
+const int N = 2010, MOD = 1e9 + 7;
+int c[N][N];
+int n, a, b;
+
+// 预处理所有的组合数的值
+void init() {
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j <= i; j++)
+            if (!j)
+                c[i][j] = 1;
+            else
+                c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % MOD;
+}
+
+void solve() {
+    init();
+
+    cin >> n;
+    while (n--) {
+        cin >> a >> b;
+        cout << c[a][b] << endl;
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    solve();
+    return 0;
+}
