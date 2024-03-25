5e239afe01f439cec7129b044fcc820f9bacb382
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2024-03-26 02:16:02 +0900

    update ACW_5560

--- 2_AcWing/99_AcWing_Week/W148/ACW_5560.cpp
+++ 2_AcWing/99_AcWing_Week/W148/ACW_5560.cpp
@@ -15,7 +15,7 @@ int lca(int a, int b) {
     // 把深度更深的放前面
     if (dep[a] < dep[b])
         swap(a, b);
-    // 逐步把a向上移动，直到a和b在同一深度
+    // 从大到小试验，把a向上移动，直到a和b在同一深度
     for (int k = K - 1; k >= 0; k--)
         if (dep[fa[a][k]] >= dep[b])
             a = fa[a][k];
@@ -38,27 +38,30 @@ int get_dist(int a, int b) {
 
 int main() {
     scanf("%d", &m);
-
+    // 初始化4个节点
     for (int i = 2; i <= 4; i++)
         fa[i][0] = 1, dep[i] = 1;
     n = 4;
-
+    // 随便设两个点A和B，距离为2
     int A = 2, B = 3, res = 2;
 
+    // 进行m次操作
     while (m--) {
+        // 给u节点扩两个节点x和y
         int u;
         scanf("%d", &u);
         int x = n + 1, y = n + 2;
         n += 2;
 
+        // 给x和y更新 dep数组 和 fa数组的值
         dep[x] = dep[y] = dep[u] + 1;
         fa[x][0] = fa[y][0] = u;
-
         for (int k = 1; k < K; k++) {
             fa[x][k] = fa[fa[x][k - 1]][k - 1];
             fa[y][k] = fa[fa[y][k - 1]][k - 1];
         }
 
+        // 计算A到x的距离，B到x的距离
         int da = get_dist(A, x), db = get_dist(B, x);
         if (da > db && da > res)
             res = da, B = x;
