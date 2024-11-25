f71b8c9ae39069c89edc34cabf0abb2b894dd69a
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2024-11-25 19:43:12 +0900

    ACW 238 Updated

--- 1_AcWing_Algo_Course/2_Advanced_Algorithms/CH4/1_Union_Find/4_ACW_238.cpp
+++ 1_AcWing_Algo_Course/2_Advanced_Algorithms/CH4/1_Union_Find/4_ACW_238.cpp
@@ -1,12 +1,15 @@
+// Problem: https://www.acwing.com/problem/content/240/
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
 typedef pair<int, int> PII;
 
-const int N = 30010;
-int m;
-// sz 表示集合的大小，d[x] 表示x到p[x]的距离
-int p[N], sz[N], d[N];
+const int N = 3e4 + 10;
+int T;
+int p[N];
+int sz[N];  // 集合的大小
+int d[N];   // x 到祖宗节点p[x]的距离
 
 int find(int x) {
     // 路径压缩
@@ -15,39 +18,44 @@ int find(int x) {
         d[x] += d[p[x]];        // 更新x到祖宗节点的距离
         p[x] = root;            // 更新x的祖宗节点
     }
-    return p[x];  // 返回x的祖宗节点
+    return p[x];  // 返回 x 的祖宗节点
 }
 
-int main() {
-    // 读入数据+并查集初始化
-    scanf("%d", &m);
-    for (int i = 1; i < N; i++) {
-        p[i] = i;
-        sz[i] = 1;
-    }
+void solve() {
+    // 并查集初始化
+    for (int i = 0; i < N; i++)
+        p[i] = i, sz[i] = 1;
 
-    while (m--) {
-        char op[2];
+    cin >> T;
+    while (T--) {
+        char op;
         int a, b;
-        scanf("%s%d%d", op, &a, &b);
-        // 合并（把a接到b后面）
-        if (op[0] == 'M') {
+        cin >> op >> a >> b;
+        // 合并（把 a 接到 b 后面）
+        if (op == 'M') {
             int pa = find(a), pb = find(b);
             if (pa != pb) {
-                d[pa] = sz[pb];    // pb的尺寸就是pa到祖宗的距离
-                sz[pb] += sz[pa];  // pa集合的尺寸加进pb中
-                p[pa] = pb;        // pb成为pa的祖宗
+                d[pa] = sz[pb];    // pb 的尺寸就是 pa 到祖宗的距离
+                sz[pb] += sz[pa];  // pa 集合的尺寸加入 pb 中
+                p[pa] = pb;        // pb 成为 pa 的祖宗
             }
         }
         // 查询
         else {
-            // 找到a和b的祖宗节点
             int pa = find(a), pb = find(b);
+            // 不在一个组
             if (pa != pb)
-                puts("-1");
+                cout << -1 << endl;
+            // 在一个组
             else
-                printf("%d\n", max(0, abs(d[a] - d[b]) - 1));
+                cout << max(0, abs(d[a] - d[b]) - 1) << endl;
         }
     }
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    solve();
     return 0;
 }
