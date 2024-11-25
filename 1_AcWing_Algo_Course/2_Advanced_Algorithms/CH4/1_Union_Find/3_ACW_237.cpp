87bd6b569df333d3f6b1f45f33c38b9d2506019c
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2024-11-25 17:51:14 +0900

    ACW 237 Updated

--- 1_AcWing_Algo_Course/2_Advanced_Algorithms/CH4/1_Union_Find/3_ACW_237.cpp
+++ 1_AcWing_Algo_Course/2_Advanced_Algorithms/CH4/1_Union_Find/3_ACW_237.cpp
@@ -1,10 +1,15 @@
+// Problem: https://www.acwing.com/problem/content/239/
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
 typedef pair<int, int> PII;
 
-const int N = 2000010;
-int n, m;
+// 注：最大有1e5个约束条件，每个条件描述2个点的关系。极端情况下需要2e5个点才够
+const int N = 2e5 + 10;
+int T;
+int cnt;  // 有 cnt 个点，（离散化用）
+int n;    // 有 n 个约束条件
 int p[N];
 unordered_map<int, int> S;
 
@@ -13,10 +18,11 @@ struct Query {
     int x, y, e;
 } query[N];
 
+// 非保序性离散化：很简单，只需要 unordered_map 就行
 // 获取x离散化后的值
 int get(int x) {
     if (S.count(x) == 0)
-        S[x] = ++n;
+        S[x] = ++cnt;
     return S[x];
 }
 
@@ -28,23 +34,24 @@ int find(int x) {
 }
 
 // 主程序
-void solve() {
-    // 初始化，并读入m条限制条件
-    n = 0;
+void answer() {
+    // 初始化，
+    cnt = 0;
     S.clear();
-    scanf("%d", &m);
-    for (int i = 0; i < m; i++) {
+    // 读入n条约束条件
+    cin >> n;
+    for (int i = 0; i < n; i++) {
         int x, y, e;
-        scanf("%d%d%d", &x, &y, &e);
+        cin >> x >> y >> e;
         query[i] = {get(x), get(y), e};
     }
 
     // 并查集初始化
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= cnt; i++)
         p[i] = i;
 
     // 合并所有的相等约束
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < n; i++) {
         if (query[i].e == 1) {
             int pa = find(query[i].x);
             int pb = find(query[i].y);
@@ -52,9 +59,9 @@ void solve() {
         }
     }
 
-    // 检查所有的不等条件
+    // 检查所有的不等约束
     bool has_conflict = false;
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < n; i++) {
         if (query[i].e == 0) {
             int pa = find(query[i].x);
             int pb = find(query[i].y);
@@ -71,10 +78,15 @@ void solve() {
         cout << "YES" << endl;
 }
 
+void solve() {
+    cin >> T;
+    while (T--)
+        answer();
+}
+
 int main() {
-    int T;
-    scanf("%d", &T);
-    while (T--) {
-        solve();
-    }
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    solve();
+    return 0;
 }
