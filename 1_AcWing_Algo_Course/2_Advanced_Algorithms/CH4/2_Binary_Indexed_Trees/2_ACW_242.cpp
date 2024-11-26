2575649f860c9f4ff1470115c9d54a4537361ed0
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2024-11-26 21:54:05 +0900

    ACW 242 Updated

--- 1_AcWing_Algo_Course/2_Advanced_Algorithms/CH4/2_Binary_Indexed_Trees/2_ACW_242.cpp
+++ 1_AcWing_Algo_Course/2_Advanced_Algorithms/CH4/2_Binary_Indexed_Trees/2_ACW_242.cpp
@@ -1,55 +1,65 @@
+// Problem: https://www.acwing.com/problem/content/248/
+
+// “区间修改，单点查询” 类型题目
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
 typedef pair<int, int> PII;
 
-const int N = 100010;
+const int N = 1e5 + 10;
 int n, m;
-int a[N];
-LL tr[N];
+int a[N];  // 储存原数组
+LL tr[N];  // 树状数组（用来储存差分数组）
 
 int lowbit(int x) {
     return x & -x;
 }
-void add(int x, int k) {
+
+// 在 x 位置加上值 c
+void add(int x, LL c) {
     for (int i = x; i <= n; i += lowbit(i))
-        tr[i] += k;
+        tr[i] += c;
 }
+
+// 求前 x 项的和
 LL sum(int x) {
     LL res = 0;
-    for (int i = x; i > 0; i -= lowbit(i))
+    for (int i = x; i; i -= lowbit(i))
         res += tr[i];
     return res;
 }
 
-int main() {
-    // 读入数据
-    scanf("%d%d", &n, &m);
+void solve() {
+    cin >> n >> m;
+    // 读入原数组
     for (int i = 1; i <= n; i++)
-        scanf("%d", &a[i]);
-    // 将差分数组整理进树状数组中
-    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+
+    // 用树状数组储存差分数组
+    for (int i = 1; i <= n; i++)
         add(i, a[i] - a[i - 1]);
-    }
 
     while (m--) {
-        // 读入操作
         char op[2];
-        scanf("%s", op);
-        // 如果操作符是 C
+        cin >> op;
+        // 如果操作为 C：
         if (op[0] == 'C') {
             int l, r, d;
-            scanf("%d%d%d", &l, &r, &d);
-            // 维护差分数组的头和尾两个点
-            add(l, d);
-            add(r + 1, -d);
+            cin >> l >> r >> d;
+            add(l, d), add(r + 1, -d);
         }
-        // 如果操作符是 Q
+        // 如果操作为 Q
         else {
             int x;
-            scanf("%d", &x);
-            // 计算差分数组的前缀和
+            cin >> x;
             cout << sum(x) << endl;
         }
     }
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    solve();
+    return 0;
 }
