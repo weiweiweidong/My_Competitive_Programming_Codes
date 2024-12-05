b0c8a60f4a0d5cf161a2c7e9685adf8c109766e7
Wei Dong <70625438+weiweiweidong@users.noreply.github.com>
2024-12-05 13:53:46 +0900

    ABC_351_D 题目使用 DFS 和 BFS 解题

---
+++ 2_AtCoder/15_Practice/DFS/3_ABC_351_D_BFS.cpp
@@ -0,0 +1,90 @@
+// Problem: https://atcoder.jp/contests/abc351/tasks/abc351_d
+
+// BFS 最大连通区域问题
+
+/*
+    本解法的关键是：将网格中'#'周围的点标记为'?'
+    那么网格中就有三种符号：
+        . 表示可以往周围的四方向走
+        # 无法走到这个位置
+        ? 走到这里就停止
+    Reference：https://atcoder.jp/contests/abc351/submissions/52864035
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+
+const int N = 1010;
+char g[N][N];   // 存储网格
+int vis[N][N];  // 记录各个点遍历的轮数
+struct node {
+    int x, y;
+};
+int h, w;
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+// 检查 (x,y) 合规性
+bool check(int x, int y, int turn) {
+    if (x < 1 || x > h || y < 1 || y > w)
+        return false;
+    if (g[x][y] == '#')
+        return false;
+    if (vis[x][y] == turn)
+        return false;
+    return true;
+}
+
+void solve() {
+    cin >> h >> w;
+    for (int i = 1; i <= h; i++)
+        for (int j = 1; j <= w; j++)
+            cin >> g[i][j];
+
+    // 把'#'周围的'.'改成'?'
+    for (int i = 1; i <= h; i++)
+        for (int j = 1; j <= w; j++)
+            if (g[i][j] == '#')
+                for (int k = 0; k < 4; k++)
+                    if (g[i + dx[k]][j + dy[k]] == '.')
+                        g[i + dx[k]][j + dy[k]] = '?';
+
+    int res = 1;
+    int turn = 0;
+    for (int i = 1; i <= h; i++)
+        for (int j = 1; j <= w; j++)
+            if (g[i][j] == '.') {
+                turn++;
+                queue<node> q;
+                q.push({i, j});
+                vis[i][j] = turn;
+                int cnt = 1;
+
+                while (q.size()) {
+                    int now_x = q.front().x, now_y = q.front().y;
+                    q.pop();
+                    if (g[now_x][now_y] == '?')
+                        continue;
+                    g[now_x][now_y] = '?';
+                    for (int k = 0; k < 4; k++) {
+                        int next_x = now_x + dx[k], next_y = now_y + dy[k];
+
+                        if (check(next_x, next_y, turn)) {
+                            q.push({next_x, next_y});
+                            vis[next_x][next_y] = turn;
+                            cnt++;
+                        }
+                    }
+                }
+                res = max(res, cnt);
+            }
+    cout << res << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    solve();
+    return 0;
+}
---
+++ 2_AtCoder/15_Practice/DFS/3_ABC_351_D_DFS.cpp
@@ -0,0 +1,90 @@
+// Problem: https://atcoder.jp/contests/abc351/tasks/abc351_d
+
+// DFS 最大连通区域问题
+
+/*
+    本解法的关键是：将网格构建成一张有向图
+    所有的格子都可以视为一个节点，坐标转换 index = xw+y
+    分成三种格子：
+        # : 直接跳过，不设置成节点
+        #周围的. : 设置成节点，但是只有入边，没有出边
+        剩余的 . : 节点，并且向四方向出边
+    Reference：https://atcoder.jp/contests/abc351/editorial/9889
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<int, int> PII;
+
+const int N = 1e6 + 10;
+int h, w;
+string g[N];       // 储存网格
+vector<int> e[N];  // 建图
+int used[N];       // 记录每个点的连通域编号，未使用为-1
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+int cnt, res;  // cnt当前连通域尺寸，res 为最终答案
+
+void dfs(int s, int u) {
+    // 如果 u 点就是现在的连通域编号，说明用过了，直接退出
+    if (used[u] == s)
+        return;
+    // 标记
+    used[u] = s;
+    cnt++;
+    // 遍历周围的点，向下继续搜
+    for (auto next : e[u])
+        dfs(s, next);
+    // 结束
+    return;
+}
+
+void solve() {
+    // 读入数据
+    cin >> h >> w;
+    for (int i = 0; i < h; i++)
+        cin >> g[i];
+
+    // 建图！！！！！！！
+    for (int x = 0; x < h; x++)
+        for (int y = 0; y < w; y++) {
+            if (g[x][y] == '#')
+                continue;
+
+            bool can = true;
+            // 将周围四个格子加入图中
+            for (int i = 0; i < 4; i++) {
+                int a = x + dx[i], b = y + dy[i];
+                if (a < 0 || a >= h || b < 0 || b >= w)
+                    continue;
+                if (g[a][b] == '#')
+                    can = false;
+                e[x * w + y].push_back(a * w + b);
+            }
+            // 如果遇到#，说明不能跳了，把边删除
+            if (!can)
+                e[x * w + y].clear();
+        }
+
+    // 初始化
+    memset(used, -1, sizeof used);
+
+    // DFS
+    for (int x = 0; x < h; x++)
+        for (int y = 0; y < w; y++) {
+            if (g[x][y] == '#')
+                continue;
+            cnt = 0;
+            dfs(x * w + y, x * w + y);
+            res = max(res, cnt);
+        }
+
+    // 输出结果
+    cout << res << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    solve();
+    return 0;
+}
